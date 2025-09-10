0b49c8bd8996dd2db2feb0a359eab8826d00391d
ti777777 <ti12272198686@yahoo.com.tw>
2025-09-10 21:00:48 +0800

    update

--- web/src/pages/Workspace/Notes/NotesPage.tsx
+++ web/src/pages/Workspace/Notes/NotesPage.tsx
@@ -99,7 +99,7 @@ const Notes = () => {
                             <div className="flex items-center gap-2 py-2 px-3 rounded-xl dark:border-neutral-600 bg-neutral-200 dark:bg-neutral-900 dark:text-neutral-100">
                                 <Search size={16} className="text-gray-400" />
                                 <input type="text" className=" flex-1 bg-transparent" placeholder={t("placeholder.search")} />
-                                <button onClick={() => setIsSearchVisible(false)}>
+                                <button title="toggle isSearchVisible" onClick={() => setIsSearchVisible(false)}>
                                     <X size={16} className="text-gray-400" />
                                 </button>
                             </div>
@@ -138,7 +138,7 @@ const Notes = () => {
                         <div className="w-full flex items-center gap-2 py-2 px-3 rounded-xl shadow-inner border dark:border-neutral-600 bg-neutral-200 dark:bg-neutral-900 dark:text-neutral-100">
                             <Search size={16} className="text-gray-400" />
                             <input type="text" className=" bg-transparent flex-1" placeholder={t("placeholder.search")} />
-                            <button onClick={() => setIsSearchVisible(false)}>
+                            <button title="toggle isSearchVisible" onClick={() => setIsSearchVisible(false)}>
                                 <X size={16} className="text-gray-400" />
                             </button>
                         </div>
