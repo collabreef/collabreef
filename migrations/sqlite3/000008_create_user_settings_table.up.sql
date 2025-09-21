6b49b6c086a42a571682cef1e471afe2c85ee63b
ti777777 <ti12272198686@yahoo.com.tw>
2025-09-21 14:56:12 +0800

    change user_id data type in user_settings table

--- migrations/sqlite3/000008_create_user_settings_table.up.sql
+++ migrations/sqlite3/000008_create_user_settings_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_settings (
-  user_id INTEGER PRIMARY KEY,
+  user_id TEXT PRIMARY KEY,
   openai_api_key TEXT,
   gemini_api_key TEXT,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
