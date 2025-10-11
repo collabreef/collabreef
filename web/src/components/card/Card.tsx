231afc3916b1c23a60aead5b0adf6afceabe4b55
ti777777 <a104221087@gmail.com>
2025-10-11 14:50:40 +0800

    add Card component and refactor ModelsPage and PreferencesPage to use it (#109)

---
+++ web/src/components/card/Card.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface Props {
+    children: ReactNode
+    className?: string
+}
+
+const Card: FC<Props> = ({ children, className }) => {
+    return <div className={twMerge(className, "bg-white dark:bg-neutral-800 rounded-xl shadow-sm p-5")}>
+        {children}
+    </div>
+}
+
+export default Card
